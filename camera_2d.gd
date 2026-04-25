fecc8ffa3a55613365ce97adeb83e3741c796eb8
VLEEHexagon <Leentje.Verfaillie@hexagon.com>
2026-04-25 14:09:09 +0200

    camera only follows y (slowly)

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,8 +1,10 @@
 extends Camera2D
 
 @onready var player = $".."
+@export var follow_speed: float = 5.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = player.global_position
+	# Smoothly follow only Y position
+	global_position.y = lerp(global_position.y, player.global_position.y, follow_speed * delta)
