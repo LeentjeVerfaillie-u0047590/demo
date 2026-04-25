c87f6fd90f67dcd0fe493d47e83e881ef395f6f5
VLEEHexagon <Leentje.Verfaillie@hexagon.com>
2026-04-25 14:16:35 +0200

    camera only follows in y-dir

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,10 +1,12 @@
 extends Camera2D
 
-@onready var player = $".."
+@onready var player = get_tree().get_first_node_in_group("player")
 @export var follow_speed: float = 5.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Smoothly follow only Y position
-	global_position.y = lerp(global_position.y, player.global_position.y, follow_speed * delta)
+	global_position.x = get_viewport_rect().size.x / 2.0
+	if player:
+		# Smoothly follow only Y position
+		global_position.y = lerp(global_position.y, player.global_position.y, follow_speed * delta)
